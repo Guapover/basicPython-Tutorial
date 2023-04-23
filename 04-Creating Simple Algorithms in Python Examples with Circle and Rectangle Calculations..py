9c237b493b0cde2c6f4a2125c797b22aa7ffaae5
mustafatrhn <96428715+Guapover@users.noreply.github.com>
2023-04-23 17:22:19 +0300

    Add files via upload

---
+++ 04-Creating Simple Algorithms in Python Examples with Circle and Rectangle Calculations..py
@@ -0,0 +1,37 @@
+#Problem: Calculate and display the area of a circle with a given radius.
+
+#To solve this problem, we will break it down into smaller parts, such as input, processing, and output.
+
+#Algorithm:
+
+#1. Create a variable to store the input (radius).
+#2. Calculate the area of the circle using the formula A = πr^2, where π is approximately equal to 3.14.
+#3. Display the result on the screen.
+
+#Example Code:
+
+radius = float(input("Please enter the radius: "))
+area = 3.14 * radius ** 2
+print("The area of the circle is: ", area)
+
+#Problem: Write a program to calculate the area and perimeter of a rectangle.
+
+#Algorithm:
+
+#1. Get input from the user for the length and width of the rectangle.
+#2. Calculate the area and perimeter of the rectangle using the formulas:
+
+area = length * width, perimeter = 2 * (length + width)
+#3. Display the area and perimeter values on the screen.
+
+#Example Code:
+
+length = float(input("Please enter the length of the rectangle: "))
+width = float(input("Please enter the width of the rectangle: "))
+
+area = length * width
+perimeter = 2 * (length + width)
+
+print("The area of the rectangle is: ", area)
+print("The perimeter of the rectangle is: ", perimeter)
+
