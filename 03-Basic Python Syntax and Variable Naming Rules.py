ce6f33fc0c1bd10fa88c25fa7a1d6a12239c77be
mustafatrhn <96428715+Guapover@users.noreply.github.com>
2023-04-23 17:15:44 +0300

    Add files via upload

---
+++ 03-Basic Python Syntax and Variable Naming Rules.py
@@ -0,0 +1,35 @@
+#We can use the + operator not only for calculations, but also for concatenating two string values.
+#print("Hello " + "Python")
+
+name = "12"
+print("My name is " + name)
+
+price = 3
+print("The apple costs" + str(price) + "dollars")
+
+count = "3"
+price = 100
+total_price = price * int(count)
+print(total_price)
+
+a=1;b=1;c=1; a=b=c=1
+
+a,b,c=3,"ali",5.2 #a,b,c=c+1,"ali",a*10
+c = a + b
+
+#     VARIABLE NAMING RULES
+
+#Variable names cannot contain spaces or operator symbols except for the _(underscore) character. Also, variable names cannot start with a number.
+#Variable names cannot be made from keywords.
+#(if, for while, else, def, in, is) keywords
+
+#Do not use long variable names! At most, use 3 words.
+
+
+#Use meaningful variable names to understand what they are used for.
+yas = 45
+
+
+# In Python, you can use characters from other languages for variable naming. However, it is recommended to use English words for code readability.
+
+# Python is case sensitive. Variables count and Count are not the same.
