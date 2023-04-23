12e957a989ada67b6987808ee96ea3b0e3dce92f
mustafatrhn <96428715+Guapover@users.noreply.github.com>
2023-04-23 17:08:34 +0300

    Add files via upload

---
+++ 02-Variable Assignment and Arithmetic Operations.py
@@ -0,0 +1,42 @@
+#Variable Assignment and Arithmetic Operations:
+
+
+name = "Mustafa"  # variable assignment
+print(name)       # printing the variable
+
+length = 5
+area = length * length  # arithmetic operation
+print(area)
+
+x = 5
+y = 10
+print(x, y)
+
+x = y
+print(x, y)
+
+x = 5
+print(x)
+
+x = x + 3  # arithmetic operation
+print(x)
+
+x = 5
+x  # printing the variable
+print(x)
+
+x += 3  # compound assignment operator
+x *= 3  # compound assignment operator, equivalent to x = x * 3
+x /= 4  # compound assignment operator, equivalent to x = x / 4
+x //= 6 # compound assignment operator, equivalent to x = x // 6
+x %= 2  # compound assignment operator, equivalent to x = x % 2
+print(x)
+
+"""
+In this code, we see variable assignment and arithmetic operations. We assign a value to a variable using the equal sign (=), and we can perform arithmetic operations with numbers and variables using the arithmetic operators (+, -, *, /, //, %).
+
+We also see the compound assignment operators (+=, *=, /=, //=, %=), which combine the arithmetic operation and the assignment operation into a single statement.
+
+We print the values of variables using the print() function, which outputs the value to the console.
+
+"""
